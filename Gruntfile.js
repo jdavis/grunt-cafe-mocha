ab688f0242bc6ced54d6e6149184a94d77c86561
Josh Davis <josh@joshldavis.com>
2013-03-23 22:17:45 -0500

    Remove unnecessary things

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,24 +23,17 @@ module.exports = function(grunt) {
             },
         },
 
-        // Before generating any new files, remove any previously-created files.
-        clean: {
-            tests: ['tmp'],
-        },
-
         // Configuration to be run (and then tested).
         cafemocha: {
             one: {
-                src: 'mocha/one.js',
+                src: 'test/*.js',
                 options: {
-                    require: ['test/require/signature'],
+                    require: [
+                        'should',
+                        'test/require/signature'
+                    ],
                 },
             },
-        },
-
-        // Unit tests.
-        nodeunit: {
-            tests: ['test/*_test.js'],
         },
 
     });
@@ -50,12 +43,10 @@ module.exports = function(grunt) {
 
     // These plugins provide necessary tasks.
     grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
     // Whenever the "test" task is run, first clean the "tmp" dir, then run this
     // plugin's task(s), then test the result.
-    grunt.registerTask('test', ['clean', 'cafemocha', 'nodeunit']);
+    grunt.registerTask('test', ['cafemocha']);
 
     // By default, lint and run all tests.
     grunt.registerTask('default', ['jshint', 'test']);
